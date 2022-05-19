58e95755c30bcee2221789981fd6b3e522e36e46
fitch520 <911727978@qq.com>
2022-05-19 19:36:15 +0800

    什么值得买编码

--- smzdm_web_sign.py
+++ smzdm_web_sign.py
@@ -71,7 +71,7 @@ if __name__ == '__main__':
     # smzdm_cookies ='__ckguid=CjA49yNCqwe7Pi7QfAPBQi7; device_id=213070643316529226901726671dcb145f0fb8df61432b7086955d4e2c; homepage_sug=d; r_sort_type=score; sajssdk_2015_cross_new_user=1; __jsluid_s=2a4e88d3a1a676a6fa4a491b5a5849c2; sess=AT-yyCOS3zK+5zGMgniIaHAYOvl730ni9S7bh+QCF3+VwGdi25yqV3VPxYxoV1y2e7vkh0RsOp5US5HKwFBmsuSfsWtK7ETBFBos4zmwNTeYw3qX4ddsqR4pVz8; user=user:4210526591|4210526591; smzdm_id=4210526591; DISABLE_APP_TIP=1; _zdmA.uid=ZDMA.OYUcjumnA.1652958595.2419200; Hm_lvt_9b7ac3d38f30fe89ff0b8a0546904e58=1652922699,1652958596; smzdm_user_view=CA4C24A3D73B6BC6FF534EB75848E555; smzdm_user_source=89ACB723A5431F5FADD8BF8F158E5499; sensorsdata2015jssdkcross={"distinct_id":"4210526591","first_id":"180d9deb33233b-0a3d0edba00e73-14333270-2073600-180d9deb333159","props":{"$latest_traffic_source_type":"直接流量","$latest_search_keyword":"未取到值_直接打开","$latest_referrer":"","$latest_landing_page":"https://www.smzdm.com/"},"$device_id":"180d9deb33233b-0a3d0edba00e73-14333270-2073600-180d9deb333159"}; Hm_lpvt_9b7ac3d38f30fe89ff0b8a0546904e58=1652958611'
     datas = smzdm_cookies.split('@')
     for cookie in datas:
-        sb.load_cookie_str(cookie)
+        sb.load_cookie_str(cookie.encode("utf-8"))
         res = sb.checkin()
         if(res['error_code'] != '0'):
             print(res['error_msg'])
